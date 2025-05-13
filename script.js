d717b999e960e3150cf8ef69742f7dd8a1b5d76d
Kartik Singh <167749570+KartikSingh09@users.noreply.github.com>
2025-05-13 13:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
-  // your code here
+    s = s.trim();
+    let spaceIndex = s.indexOf(" ");
+    return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
